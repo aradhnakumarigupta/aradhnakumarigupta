acca904fbbd45baac602f8e88de11065c9151316
aradhnakumarigupta <106858105+aradhnakumarigupta@users.noreply.github.com>
2022-06-21 23:14:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,64 @@
+var path,boy, leftBoundary,rightBoundary;
+var pathImg,path;
+
+function preload(){
+  pathImg = loadImage("path.png");
+  boyImg = loadAnimation("Runner-1.png","Runner-2.png");
+}
+
+function setup(){
+  
+  createCanvas(400,400);
+  
+// Moving background
+path=createSprite(200,200);
+path.addImage(pathImg);
+path.velocityY = 4;
+path.scale=1.2;
+
+//creating boy running
+boy = createSprite(180,340,30,30);
+boy.scale=0.08;
+boy.addAnimation("JakeRunning",boyImg);
+  
+
+leftBoundary=createSprite(0,0,100,800);
+ leftBoundary.visible = false;
+
+
+rightBoundary=createSprite(410,0,100,800);
+rightBoundary.visible = false;
+}
+
+function draw() {
+  background(0);
+  path.velocityY = 4;
+ 
+  //creating invisible path
+  invisiblepath = createSprite(200,180,400,20);
+  invisiblepath.visible= false;
+
+  boy.x = World.mouseX;
+  edges= createEdgeSprites();
+  boy.collide(edges[3]);
+  boy.collide(leftBoundary);
+  boy.collide(rightBoundary);
+  
+  //code to reset the background
+
+  if(path.y > 400 ){
+    path.y = height/-2;
+  } 
+
+  /*if(path.y > 400 ){
+   
+ path.y = height/2;
+  }*/
+
+  /*if(path.y > 400 ){
+path.y = height/2;}*/
+
+/*if(path.y > 400 ){path.y = height/2;}*/
+  
+  drawSprites();
+}
